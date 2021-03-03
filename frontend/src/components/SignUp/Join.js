c92d76c8a25976adfd402187cc5b80c7f43190d7
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-03-04 01:48:13 +0900

    Implement Sign-up

--- frontend/src/components/SignUp/Join.js
+++ frontend/src/components/SignUp/Join.js
@@ -1,18 +1,103 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import TermPopup from './TermPopup';
+import MyTheme from '../Common/MyTheme';
 
-const Join = () => {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(5),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Join() {
+  const classes = useStyles();
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '98vh'
-      }}
-    >
-      <h1>Sign Up</h1>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <ThemeProvider theme={MyTheme}>
+          <Typography variant="body1">
+            Join LOGO as a Recipient
+          </Typography>
+          <br />
+          <Typography variant="h1">
+            Create Account
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              name="username"
+              autoComplete="uname"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="agreeTerms" color="secondary" />}
+              label={<div>I Agree to &nbsp;<TermPopup/></div>}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="secondary"
+              className={classes.submit}
+            >
+              Sign Up
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="/Login" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </ThemeProvider>
+      </div>
+    </Container>
   );
 }
-
-export default Join;
