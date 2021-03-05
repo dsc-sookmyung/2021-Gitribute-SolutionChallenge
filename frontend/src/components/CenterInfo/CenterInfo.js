b155fd1d1e73a2170897a519e3dfea3659098bba
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-03-06 02:30:26 +0900

    Implement Choose your local LOGO center

--- frontend/src/components/CenterInfo/CenterInfo.js
+++ frontend/src/components/CenterInfo/CenterInfo.js
@@ -1,18 +1,116 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { Radio, RadioGroup, FormControl, FormControlLabel } from '@material-ui/core';
+import CenterTable from './CenterTable';
+import Container from '@material-ui/core/Container';
 
-const CenterInfo = () => {
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: '2.5rem calc((100vw - 1193px) / 2 + 1rem)',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    '@media screen and (max-width: 1193px)': {
+      padding: '2.5rem 1rem'
+    },
+  },
+  select: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(6),
+  },
+  table: {
+    maxWidth: "1194px",
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(5),
+  },
+  submit: {
+    margin: theme.spacing(6, 0, 2),
+  },
+}));
+
+export default function CenterInfo() {
+  const classes = useStyles();
+
+  const changeLocation = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '98vh'
-      }}
-    >
-      <h1>CenterInfo</h1>
-    </div>
+    <Container component="main" className={classes.paper}>
+      <Typography variant="h3">
+        Search by location
+      </Typography>
+      <Grid container className={classes.select}>
+        <FormControl component="fieldset">
+        <RadioGroup 
+          row aria-label="location" 
+          onChange={changeLocation}
+          className={classes.radio}  
+        >
+          <Grid item xs={6} sm={3}>
+          <FormControlLabel
+            value="전체"
+            control={<Radio />}
+            label="전체"
+          />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+          <FormControlLabel
+            value="서울" 
+            control={<Radio />}
+            label="서울"
+          />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+          <FormControlLabel
+            value="경기/인천" 
+            control={<Radio />}
+            label="경기/인천"
+          />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+          <FormControlLabel
+            value="충남/세종/대전/강원" 
+            control={<Radio />}
+            label="충남/세종/대전/강원"
+          />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+          <FormControlLabel
+            value="전라/광주" 
+            control={<Radio />}
+            label="전라/광주"
+          />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+          <FormControlLabel
+            value="경상/대구/부산/울산" 
+            control={<Radio />}
+            label="경상/대구/부산/울산"
+          />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+          <FormControlLabel
+            value="제주" 
+            control={<Radio />}
+            label="제주"
+          />
+          </Grid>
+          <Grid item xs={6} sm={3}>
+          <FormControlLabel 
+            value="disabled" 
+            disabled control={<Radio />} 
+            label="지원하지 않는 지역" />
+          </Grid>
+        </RadioGroup>
+        </FormControl>
+      </Grid>
+      <CenterTable className={classes.table} />
+    </Container>
   );
 }
-
-export default CenterInfo;
