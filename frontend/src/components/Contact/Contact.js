00b9b0ba318f7fd310e2951ca642068774de7e7a
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-03-06 02:13:30 +0900

    Implement Contact

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -1,17 +1,87 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Profile from './Profile';
+
+import suhee from '../../Images/0hee0.jpg';
+import eunji from '../../Images/heleneunji.jpg';
+import jiyeon from '../../Images/hellouz818.png';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: '2.5rem calc((100vw - 1193px) / 2 + 1rem)',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    '@media screen and (max-width: 1193px)': {
+      padding: '2.5rem 1rem'
+    },
+  },
+  title: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(3)
+  },
+  profile: {
+    paddingTop: theme.spacing(3),
+    display: 'flex',
+    jutifyContent: 'space-between',
+    flexWrap: 'wrap',
+
+    '@media only screen and (max-width: xs)': {
+        width: '100%',
+        margin: 0
+    }
+  },
+  hr: {
+    [theme.breakpoints.up('md')]: {
+      width: "1194px",
+    }, 
+    width: "calc(100vw - 2rem)",
+    border: "solid 0.1px #e9ecef", 
+    transform: "scaleY(0.5)", 
+  },
+}))
 
 const Contact = () => {
+  const classes = useStyles();
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '98vh'
-      }}
-    >
-      <h1>Contact</h1>
-    </div>
+    <Container component="main" className={classes.paper}>
+      <Typography variant="h3" >
+        Developers
+      </Typography>
+      <div className={classes.profile}>
+        <Profile
+          name={"서희"}
+          email={"suhee0206@gmail.com"}
+          image={suhee}
+          github={"0hee0"}
+          role={"front-end"}
+        />
+        <Profile 
+          name={"권은지"}
+          email={"email"}
+          image={eunji}
+          github={"heleneunji"}
+          role={"back-end"} 
+        />
+        <Profile 
+          name={"유지연"}
+          email={"email"}
+          image={jiyeon}
+          github={"hellouz818"}
+          role={"back-end"}
+        />
+      </div>
+      <hr className={classes.hr} />
+      <Typography variant="h3" className={classes.title}>
+        Contact Us
+      </Typography>
+      ✉ &nbsp; <strong>Email</strong> <br/ >
+      sookmyung.ac.kr
+    </Container>
   );
 }
 
---
+++ frontend/src/components/Contact/Profile.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import GitHubIcon from '@material-ui/icons/GitHub';
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+
+const Profile = ({ name, email, image, github, role }) => {
+  const StyledProfile = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 14rem;
+
+    @media only screen and (max-width: 768px) {
+      width: 16rem;
+    }
+  `;
+
+  const DescWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding-top: 0.6rem;
+    padding-bottom: 1.2rem;
+    height: 76%;
+  `;
+
+  const Info = styled.div`
+    font-size: 0.8rem;
+    align-items: center;
+    display: flex;
+    color: #666666;
+    line-height: 1;
+
+    @media only screen and (max-width: 768px) {
+      font-size: 1rem;
+    }
+
+  `;
+
+  const ProfileImage = styled.img`
+    border-radius: 50%;
+    width: 8rem;
+    height: 8rem;
+    object-fit: cover;
+    margin-right: 0.8rem;
+  `;
+
+  const Name = styled.h3`
+    margin: 0.25rem 0;
+    font-size: 1rem;
+    &:after {
+      content: ' ${props => props.role}';
+      font-size: 0.75rem;
+      color: #444444;
+    }
+  `;
+
+    return (
+      <StyledProfile>
+        <ProfileImage src={image} alt="profile image"/>
+        <DescWrapper>
+          <Name role={role}>{name}</Name>
+          <Info><GitHubIcon fontSize="small" style={{transform: "scale(0.6)"}}/>{github}</Info>  
+          <Info><MailOutlineIcon fontSize="small" style={{transform: "scale(0.6)"}} />{email}</Info>
+        </DescWrapper>
+      </StyledProfile>
+    );
+}
+
+export default Profile;
