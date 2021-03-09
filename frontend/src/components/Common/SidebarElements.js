75dc947a4cb25c5c7e5fa4428a3398b382c52c10
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-03-10 01:49:15 +0900

    Add scroll and SidebarLabel style

--- frontend/src/components/Common/SidebarElements.js
+++ frontend/src/components/Common/SidebarElements.js
@@ -10,7 +10,7 @@ export const NavIcon = styled(Link)`
 `;
 
 export const SidebarNav = styled.nav`
-  background: #fff;
+  background: #f3e5f5;
   width: 232px;
   height: 100vh;
   display: flex;
@@ -21,6 +21,19 @@ export const SidebarNav = styled.nav`
   transition: 350ms;
   z-index: 10;
   box-shadow: 3px 3px 3px 1px rgba(0, 0, 0, 0.1);
+
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    width: 16px;
+  }
+  ::-webkit-scrollbar-thumb {
+      height: 6px;
+      border: 4px solid rgba(0, 0, 0, 0);
+      background-clip: padding-box;
+      -webkit-border-radius: 7px;
+      background-color: #fff;
+      -webkit-box-shadow: inset -1px -1px 0px rgba(0, 0, 0, 0.05), inset 1px 1px 0px rgba(0, 0, 0, 0.05);
+  }
 `;
 
 export const SidebarWrap = styled.div`
@@ -38,8 +51,39 @@ export const SidebarLink = styled(Link)`
   text-decoration: none;
   font-size: 1rem;
   &:hover {
-    background: #f3e5f5;
-    border-left: 4px solid #9c27b0;    // purple[500]
+    background: #fff;
+    border-left: 4px solid #9c27b0;
     cursor: pointer;
   }
+  &:focus {
+    background: #fff;
+    border-left: 4px solid #9c27b0;
+  }
+
 `;
+
+export const SidebarTitleLabel = styled.div`
+  display: flex;
+  color: #000;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 1.25rem;
+  list-style: none;
+  text-decoration: none;
+  font-size: 1rem;
+  font-weight: bold;
+  background: rgba(255, 255, 255, 0.7);
+`;
+
+export const SidebarLabel = styled.div`
+  display: flex;
+  color: #000;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5rem 1.25rem;
+  list-style: none;
+  text-decoration: none;
+  font-size: 1rem;
+  background: rgba(255, 255, 255, 0.7);
+`;
+
