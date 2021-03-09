5e5956848eb79f063bc5ac5a984cd8e5fb6f6b82
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-03-10 01:48:05 +0900

    Add UserInfo to Sidebar

--- frontend/src/components/Common/Sidebar.js
+++ frontend/src/components/Common/Sidebar.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react';
-import { NavIcon, SidebarNav, SidebarWrap, SidebarLink } from './SidebarElements';
+import { NavIcon, SidebarNav, SidebarWrap, SidebarLink, SidebarTitleLabel, SidebarLabel } from './SidebarElements';
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
 import { BarData } from './BarData';
+import UserInfo from './UserInfo';
 
 const Sidebar = () => {
   const [sidebar, setSidebar] = useState(false);
+  const [role, setRole] = useState(2);
+  const [dropBox, setDropBox] = useState(false);
 
   const showSidebar = () => setSidebar(!sidebar);
+  const showdropBox = () => setDropBox(!dropBox);
 
   return (
     <>
@@ -24,6 +28,32 @@ const Sidebar = () => {
             </SidebarLink>
             );
           })}
+          { role === 0 ? (
+            <SidebarLink to='/login' activeStyle>Sign&nbsp;In</SidebarLink>
+          ) : (
+            <SidebarLink to='#' onClick={showdropBox} activeStyle>User</SidebarLink>
+          )}
+          { dropBox ? (
+            role === 1 ? (
+              <div className="modal">
+                <SidebarTitleLabel>Available</SidebarTitleLabel>
+                <SidebarLabel>Total &nbsp;&nbsp; 8</SidebarLabel>
+                <SidebarTitleLabel>Sign Out</SidebarTitleLabel>
+              </div>           
+            ) : (
+              role === 2 ? (
+                <div className="modal">
+                  <SidebarTitleLabel>Level</SidebarTitleLabel>
+                  <SidebarLabel>🌱 7</SidebarLabel>
+                  <SidebarTitleLabel>Donation</SidebarTitleLabel>
+                  <SidebarLabel>Panty Liner 3</SidebarLabel>
+                  <SidebarLabel>Medium 1</SidebarLabel>
+                  <SidebarLabel>Large 2</SidebarLabel>
+                  <SidebarLabel>Overnight 1</SidebarLabel>
+                  <SidebarTitleLabel>Sign Out</SidebarTitleLabel>
+                </div>           
+              ) : null )
+          ) : null }
         </SidebarWrap>
       </SidebarNav>
     </>
