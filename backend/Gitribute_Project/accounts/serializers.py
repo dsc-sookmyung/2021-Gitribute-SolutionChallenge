edbbec14f0a443ace386a0df1a1634fc9ade6d4a
hellouz818 <49220095+hellouz818@users.noreply.github.com>
2021-03-12 19:32:44 +0900

    Modify Accounts App with ImageField

---
+++ backend/Gitribute_Project/accounts/serializers.py
@@ -0,0 +1,109 @@
+from rest_framework import serializers
+from rest_framework_jwt.settings import api_settings
+from django.contrib.auth import get_user_model
+from .models import User
+from django.contrib.auth.models import update_last_login
+from django.contrib.auth import authenticate
+
+JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
+JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
+
+User = get_user_model()
+
+class DonorCreateSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    role = serializers.IntegerField(required=True)
+    center = serializers.CharField(required=False)
+    
+    #Donor
+    level = serializers.IntegerField(required=False)
+    liner = serializers.IntegerField(required=False)
+    medium = serializers.IntegerField(required=False)
+    large = serializers.IntegerField(required=False)
+    overnight = serializers.IntegerField(required=False)
+    
+    #Receiver
+    #image_profile = serializers.ImageField(required=False)
+    #total = serializers.IntegerField(required=False)
+    
+
+    def create(self, validated_data):
+        donor = User.objects.create(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            role=validated_data['role'],
+            center=None,
+            level = 1,
+            liner = 0,
+            medium = 0,
+            large = 0,
+            overnight = 0,
+        )
+        donor.set_password(validated_data['password'])
+
+        donor.save()
+        return donor
+   
+class ReceiverCreateSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    role = serializers.IntegerField(required=True)
+    center = serializers.CharField(required=False)
+    
+    #Donor
+    #level = serializers.IntegerField(required=False)
+    #liner = serializers.IntegerField(required=False)
+    #medium = serializers.IntegerField(required=False)
+    #large = serializers.IntegerField(required=False)
+    #overnight = serializers.IntegerField(required=False)
+    
+    #Receiver
+    image_profile = serializers.ImageField(required=True)
+    total = serializers.IntegerField(required=False)
+    
+
+    def create(self, validated_data):
+        receiver = User.objects.create(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            role=validated_data['role'],
+            image_profile=validated_data['image_profile'],
+            center=None,
+            total=10,
+        )
+        receiver.set_password(validated_data['password'])
+
+        receiver.save()
+        return receiver
+   
+    
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=64)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        email = data.get("email", None)
+        password = data.get("password", None)
+        user = authenticate(email=email, password=password)
+
+        if user is None:
+            return {
+                'email': 'None'
+            }
+        try:
+            payload = JWT_PAYLOAD_HANDLER(user)
+            jwt_token = JWT_ENCODE_HANDLER(payload)
+            update_last_login(None, user)
+        except User.DoesNotExist:
+            raise serializers.ValidationError(
+                'User with given email and password does not exists'
+            )
+        return {
+            'email': user.email,
+            'token': jwt_token
+        }
--- backend/Gitribute_Project/accounts/views.py
+++ backend/Gitribute_Project/accounts/views.py
@@ -1,35 +1,52 @@
 from django.shortcuts import render
-from rest_framework import status
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
-from .serializers import UserCreateSerializer
-from .serializers import UserLoginSerializer
+from .serializers import DonorCreateSerializer, ReceiverCreateSerializer, UserLoginSerializer
 from .models import User
 
-@api_view(['POST']) 
-@permission_classes([AllowAny]) # 인증 필요없다
-def signup(request):
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def createUser(request):
     if request.method == 'POST':
-        serializer = UserCreateSerializer(data=request.data)
+        num = request.data["role"]
+        print(num)
+
+        #num이 1이면 Donor, 2이면 Receiver
+        if int(num) == 1:
+            print("donor시리얼라이저")
+            serializer = DonorCreateSerializer(data=request.data)
+        elif int(num) == 2:
+            print("receiver시리얼라이저")
+            serializer = ReceiverCreateSerializer(data=request.data)
+        else:
+            print("validated_serializer 오류")
+
+        
         if not serializer.is_valid(raise_exception=True):
             return Response({"message": "Request Body Error."}, status=status.HTTP_409_CONFLICT)
 
         if User.objects.filter(email=serializer.validated_data['email']).first() is None:
+            
             serializer.save()
+            print(serializer.data)
             return Response({"message": "ok"}, status=status.HTTP_201_CREATED)
-        return Response({"message": "duplicate email"}, status=status.HTTP_409_CONFLICT) 
+        return Response({"message": "duplicate email"}, status=status.HTTP_409_CONFLICT)
+
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def login(request):
     if request.method == 'POST':
+        u=request.data
+        print(u)
         serializer = UserLoginSerializer(data=request.data)
-
+        
         if not serializer.is_valid(raise_exception=True):
             return Response({"message": "Request Body Error."}, status=status.HTTP_409_CONFLICT)
-        if serializer.validated_data['nickname'] == "None":
+        if serializer.validated_data['email'] == "None":
             return Response({'message': 'fail'}, status=status.HTTP_200_OK)
 
         response = {
