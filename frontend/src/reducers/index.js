801ebec51b0dd0462f0750778f21e399b70a3e2b
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-03-15 20:58:43 +0900

    Add User state management

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import errors from './Errors';
 import messages from './Messages';
 import auth from './Auth';
+import user from './User';
 
 export default combineReducers({
   errors,
   messages,
   auth,
+  user,
 });
