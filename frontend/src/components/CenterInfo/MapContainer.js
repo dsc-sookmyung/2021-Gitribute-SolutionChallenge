ce297a48618cfe10fabe87c771dbdbcfa3f8932d
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-03-20 00:23:12 +0900

    Add Google Map API

---
+++ frontend/src/components/CenterInfo/MapContainer.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const MapContainer = ({ lat, lng, handleMarker, role, showForm }) => {
+  const mapStyles = {
+    height: "22.4rem",
+    width: "100%"
+  }
+  
+  const recipientMapStyles = {   
+    height: "24.4rem",
+    width: "100%"
+  }
+
+  const showFormStyles = {
+    height: "8.8rem",
+    width: "100%"
+  }
+
+  const reShowFormStyles = {
+    height: "20rem",
+    width: "100%"
+  }
+
+  const defaultCenter = {
+    lat: 41.3851, lng: 2.1734
+  }
+  
+  if (showForm) {
+    var mapStyle = showFormStyles
+    if (role === 1) {
+      mapStyle = reShowFormStyles
+    }
+  }
+  else {
+    var mapStyle = mapStyles
+    if (role === 1) {
+      mapStyle = recipientMapStyles
+    }
+  }
+
+
+  return (
+     <LoadScript
+       googleMapsApiKey='AIzaSyCIYlorAfLUWWY5gyB47xPjZ1lclWZwHWQ'>
+        <GoogleMap
+          mapContainerStyle={mapStyle}
+          zoom={20}
+          center={{lat: lat, lng: lng}}
+        >
+          <Marker position={{lat: lat, lng: lng}} onClick={handleMarker} />
+        </GoogleMap>
+     </LoadScript>
+  )
+}
+
+export default MapContainer;
