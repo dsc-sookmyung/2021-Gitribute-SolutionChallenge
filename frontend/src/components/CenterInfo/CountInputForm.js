670847790e956e38705875f7fb3cab98cec62f1a
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-03-21 04:00:29 +0900

    Add original number state & confirmation popup

--- frontend/src/components/CenterInfo/CountInputForm.js
+++ frontend/src/components/CenterInfo/CountInputForm.js
@@ -1,11 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import AuthService from '../../services/auth.service';
+import UserService from '../../services/user.service';
+
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { Checkbox, FormControlLabel, Input, Button, ButtonGroup } from '@material-ui/core';
-import styled from 'styled-components';
+import { Checkbox, FormControlLabel, Input, Button, ButtonGroup, Tab } from '@material-ui/core';
+import { Table, TableHead, TableBody, TableRow } from '@material-ui/core';
+import MuiTableCell from "@material-ui/core/TableCell";
+import Popup from "reactjs-popup";
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    '@media screen and (max-width: 1193px)': {
+      // padding: 2.5rem 1rem;
+    }
+  },
   select: {
     marginTop: theme.spacing(1),
     marginBottom: theme.spacing(1.5),
@@ -15,42 +28,151 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: theme.spacing(1)
   },
   input: {
-    width: "3rem",
+    width: "4rem",
     marginBottom: theme.spacing(1),
     marginRight: theme.spacing(1)
-  }
+  },
+  popupTop: {
+    display: "flex",
+    marginTop: theme.spacing(3)
+  },
+  popupTitle: {
+    flex: "0 1 auto",
+    position: "absolute",
+    left: "50%",
+    transform: "translateX(-50%)"
+  },
+  /*
+  popupTop: {
+  display: "flex",
+  justifyContent: "flex-start",   // adjustment
+  position: "relative",
+  alignItems: "center"
+  },
+  popupTitle: {
+    flex: "0 1 auto",
+    position: "absolute",
+    left: "50%",
+    transform: "translateX(-50%)"
+  },
+  popupClose: {
+    flex: "0 1 auto",
+    marginLeft: "auto"
+  },
+  */
+  popupContent: {
+    padding: "2rem"
+  },
+  popupButton: {
+    display: "flex",
+    alignItems: "center",
+    marginBottom: theme.spacing(3)
+  },
+  popupCancel: {
+    marginLeft: "auto",
+    marginRight: "1.5rem",
+  },
+  popupSubmit: {
+    marginRight: "2rem"
+  },
 }));
 
-export default function CenterInfo({ role, handlePad }) {
+const TableCell = withStyles({
+  root: {
+    borderBottom: "none"
+  }
+})(MuiTableCell);
+
+const contentStyle = {
+  maxWidth: "90vw",
+  width: "34vw",
+  background: "#fff",
+  boxShadow: "3px 5px 5px 3px rgba(0, 0, 0, 0.1)"
+};
+
+export default function CountInputForm({ role, center }) {
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [total, setTotal] = useState(0);
   const [checkedType, setCheckedType] = useState([]);
+  const [originalLiner, setOriginalLiner] = useState(0);
+  const [originalMedium, setOriginalMedium] = useState(0);
+  const [originalLarge, setOriginalLarge] = useState(0);
+  const [originalOvernight, setOriginalOvernight] = useState(0);
   const [linerCounter, setLinerCounter] = useState(0);
   const [mediumCounter, setMediumCounter] = useState(0);
   const [largeCounter, setLargeCounter] = useState(0);
   const [overnightCounter, setOvernightCounter] = useState(0);
+  const [limit, setLimit] = useState(false);
   const classes = useStyles();
 
   useEffect(() => {
-    console.log(checkedType);
-  }, [checkedType, linerCounter, mediumCounter, largeCounter, overnightCounter]);
+    const user = AuthService.getCurrentUser();
+
+    if (user) {
+      setCurrentUser(user);
+      
+      if (user.total) {
+        setTotal(user.total);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (total) {
+      const totalCounter = linerCounter + mediumCounter + largeCounter + overnightCounter;
+      if (totalCounter > total) {
+        setLimit(true);
+        alert(`You have exceeded the number you can take!\nNumber of sanitary pads you can take: ${total}`);
+      }  
+    }
+  }, [linerCounter, mediumCounter, largeCounter, overnightCounter]);
 
   const handlePadType = ((e) => {
     setCheckedType({...checkedType, [e.target.name] : e.target.checked });
   });
 
+  const handleOriginalLiner = (e) => {
+    setOriginalLiner(e.target.value);
+  }
+
+  const handleOriginalMedium = (e) => {
+    setOriginalMedium(e.target.value);
+  }
+
+  const handleOriginalLarge = (e) => {
+    setOriginalLarge(e.target.value);
+  }
+
+  const handleOriginalOvernight = (e) => {
+    setOriginalOvernight(e.target.value);
+  }
+
   const handleLinerIncrement = () => {
-    setLinerCounter(prevLinerCounter => prevLinerCounter + 1);
+    if (!limit) {
+      setLinerCounter(prevLinerCounter => prevLinerCounter + 1);
+    }
+    setLimit(false);
   }
 
   const handleMediumIncrement = () => {
-    setMediumCounter(prevMediumCounter => prevMediumCounter + 1);
+    if (!limit) {
+      setMediumCounter(prevMediumCounter => prevMediumCounter + 1);
+    }
+    setLimit(false);
   }
 
   const handleLargeIncrement = () => {
-    setLargeCounter(prevLargeCounter => prevLargeCounter + 1)
+    if (!limit) {
+      setLargeCounter(prevLargeCounter => prevLargeCounter + 1);
+    }
+    setLimit(false);
   }
 
   const handleOvernightIncrement = () => {
-    setOvernightCounter(prevOvernightCounter => prevOvernightCounter + 1)
+    if (!limit) {
+      setOvernightCounter(prevOvernightCounter => prevOvernightCounter + 1);
+    }
+    setLimit(false);
   }
 
   const handleLinerDecrement = () => {
@@ -70,11 +192,23 @@ export default function CenterInfo({ role, handlePad }) {
   }
 
   const handleSubmit = () => {
-
+    const original = {
+      originalLiner,
+      originalMedium,
+      originalLarge,
+      originalOvernight
+    };
+    const padCount = {
+      linerCounter,
+      mediumCounter,
+      largeCounter,
+      overnightCounter
+    };
+    UserService.handlePad(linerCounter, mediumCounter, largeCounter, overnightCounter);
   }
 
   return (
-    <Container>
+    <div className={classes.container}>
       { role === 2 ? (
         <strong>❤ Choose the types of sanitary pad you brought</strong>
       ) : (
@@ -117,7 +251,9 @@ export default function CenterInfo({ role, handlePad }) {
             <Typography className={classes.label} variant="body2">Panty Liner</Typography>
             <Input 
               className={classes.input}
-              type="number" />
+              type="number"
+              onChange={handleOriginalLiner}
+              placeholder="original" />
             <ButtonGroup size="small">
               { linerCounter > 0 ? (
                 <Button onClick={handleLinerDecrement}>-</Button>
@@ -134,7 +270,9 @@ export default function CenterInfo({ role, handlePad }) {
           <Typography className={classes.label} variant="body2">Medium</Typography>
           <Input 
             className={classes.input}
-            type="number" />
+            type="number" 
+            onChange={handleOriginalMedium} 
+            placeholder="original" />
           <ButtonGroup size="small">
             { mediumCounter > 0 ? (
               <Button onClick={handleMediumDecrement}>-</Button>
@@ -151,7 +289,9 @@ export default function CenterInfo({ role, handlePad }) {
           <Typography className={classes.label} variant="body2">Large</Typography>
           <Input 
             className={classes.input}
-            type="number" />
+            type="number" 
+            onChange={handleOriginalLarge}
+            placeholder="original" />
           <ButtonGroup size="small">
             { largeCounter > 0 ? (
               <Button onClick={handleLargeDecrement}>-</Button>
@@ -168,7 +308,9 @@ export default function CenterInfo({ role, handlePad }) {
           <Typography className={classes.label} variant="body2">Overnight</Typography>
           <Input 
             className={classes.input}
-            type="number" />
+            type="number" 
+            onChange={handleOriginalOvernight}
+            placeholder="original" />
           <ButtonGroup size="small">
             { overnightCounter > 0 ? (
               <Button onClick={handleOvernightDecrement}>-</Button>
@@ -181,26 +323,93 @@ export default function CenterInfo({ role, handlePad }) {
         </Grid>
         ) : null }  
         </Grid>    
-      <Button
-          onClick={handleSubmit}
-          type="submit"
-          fullWidth
-          variant="contained"
-          color="secondary"
-          size="small"
-          className={classes.submit}
+        <Popup
+        trigger={
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            size="small"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+        }
+        modal
+        contentStyle={contentStyle}
         >
-        Submit
-        </Button>
-    </Container>
+          {close => (
+            <>
+            <div className={classes.popupTop}>
+              <Typography variant="h3" className={classes.popupTitle}>Are you sure?</Typography>
+            </div>
+            <div className={classes.popupContent}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <MuiTableCell>Type</MuiTableCell>
+                    <MuiTableCell>Previous</MuiTableCell>
+                    <MuiTableCell>You</MuiTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {checkedType["liner"] ? (
+                    <TableRow>
+                      <TableCell>Panty Liner</TableCell>
+                      <TableCell>{originalLiner}</TableCell>
+                      <TableCell>{linerCounter}</TableCell>
+                    </TableRow>
+                  ) : null}
+                  {checkedType["medium"] ? (
+                    <TableRow>
+                      <TableCell>Medium</TableCell>
+                      <TableCell>{originalMedium}</TableCell>
+                      <TableCell>{mediumCounter}</TableCell>
+                    </TableRow>
+                  ) : null}
+                  {checkedType["large"] ? (
+                    <TableRow>
+                      <TableCell>Large</TableCell>
+                      <TableCell>{originalLarge}</TableCell>
+                      <TableCell>{largeCounter}</TableCell>
+                    </TableRow>
+                  ) : null}
+                  {checkedType["overnight"] ? (
+                    <TableRow>
+                      <TableCell>Overnight</TableCell>
+                      <TableCell>{originalOvernight}</TableCell>
+                      <TableCell>{overnightCounter}</TableCell>
+                    </TableRow>
+                  ) : null}
+                </TableBody>
+              </Table>
+            </div>
+            <div className={classes.popupButton}>
+              <Button 
+                className={classes.popupCancel}
+                onClick={() => {close()}}
+                size="small"
+                variant="outlined"
+                color="secondary"
+              >
+                Cancel
+              </Button>
+              <Button 
+                className={classes.popupSubmit}
+                onClick={() => {
+                  handleSubmit(); 
+                  close(); }}
+                size="small"
+                variant="contained"
+                color="secondary"
+              >
+                Submit
+              </Button>
+            </div>
+            </>
+          )}
+        </Popup>
+    </div>
   );
 }
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  @media screen and (max-width: 1193px) {
-    // padding: 2.5rem 1rem;
-  }
-`;
