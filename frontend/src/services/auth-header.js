20656b1d713cb251202fc9ab34cbb7b596474f9e
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-03-21 03:57:35 +0900

    Fix unauthorized problem

--- frontend/src/services/auth-header.js
+++ frontend/src/services/auth-header.js
@@ -2,7 +2,7 @@ export default function authHeader() {
   const user = JSON.parse(localStorage.getItem('user'));
 
   if (user && user.token) {
-    return { Authorization: 'Bearer ' + user.token };
+    return { Authorization: 'JWT '+user.token };
   } else {
     return {};
   }
--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -3,6 +3,7 @@ import authHeader from "./auth-header";
 
 const API_URL_MYPAGE = "http://localhost:8000/users/mypage/";
 const API_URL_CENTER = "http://localhost:8000/center/";
+const API_URL_STAR = "http://localhost:8000/scrap/center/";
 
 const getUserInfo = () => {
   return axios
@@ -14,7 +15,7 @@ const getUserInfo = () => {
 
 const getDefaultCenter = (region) => {
   return axios
-    .post(API_URL_CENTER, region, { headers: authHeader() })
+    .put(API_URL_CENTER, region, { headers: authHeader() })
     .then((response) => {
       return JSON.parse(response.data);  
     });
@@ -22,7 +23,7 @@ const getDefaultCenter = (region) => {
 
 const getCenter = (region, center) => {
   return axios
-    .post(API_URL_CENTER, {
+    .put(API_URL_CENTER, {
       region, 
       center
     }, { headers: authHeader() })
@@ -31,25 +32,38 @@ const getCenter = (region, center) => {
     })
 };
 
-const handlePad = (center, padCount) => {
-  return axios
-    .post(API_URL_MYPAGE, {
-      center, 
-      padCount
-    }, { headers: authHeader() })
-    .then((response) => {
-      return JSON.parse(response.data);
-    })
+const handlePad = (linerCounter, mediumCounter, largeCounter, overnightCounter) => {
+  return axios({
+    method: 'put',
+    url: API_URL_MYPAGE,
+    headers: authHeader(),
+    data: {
+      liner: linerCounter,
+      medium: mediumCounter,
+      large: largeCounter,
+      overnight: overnightCounter
+    }
+  })
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+    return JSON.parse(response.data);
+  })
 };
 
 // ADD DELETE STAR
 const handleStar = (star) => {
-  return axios
-    .post(API_URL_MYPAGE, star, { headers: authHeader() })
-    .then((response) => {
-      alert(response.data);
-      return JSON.parse(response.data);
-    })
+  return axios({
+    method: 'put',
+    url: API_URL_STAR,
+    headers: authHeader(),
+    data: {
+      center: star
+    }
+  })
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+    return JSON.parse(response.data);
+  })
 };
 
 export default {
