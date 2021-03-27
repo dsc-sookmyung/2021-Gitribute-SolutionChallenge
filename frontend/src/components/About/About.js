52fff4a4db05ddf125358a3e76c283f684206bb5
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-03-27 22:37:14 +0900

    Add landing page

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,16 +1,55 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import image from '../../assets/images/high-five.png';
+import createSpacing from '@material-ui/core/styles/createSpacing';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    backgroundColor: "#fff",
+    height: "100vh",
+    padding: "2.5rem calc((100vw - 1193px) / 2 + 1rem)",
+    display: "flex",
+    alignItems: "center",
+    // justifyContent: "space-around",
+    '@media screen and (max-width: 1193px)': {
+      padding: "2.5rem 1rem"
+    },
+    '@media screen and (max-width:767px)': {
+      padding: "2.5rem 2rem",
+      flexDirection: "column"
+    },
+  },
+  about: {
+    marginRight: "auto",
+    alignSelf: "center",
+    '@media (max-width:767px)': {
+      display: "flex",
+      flexDirection: "column",
+      alignSelf: "flex-start",
+      paddingBottom: "4rem"
+    }
+  },
+  imageContainer: {
+    alignSelf: "center",
+    width: "400px",
+    '@media (max-width:767px)': {
+      width: "260px"
+    }
+  }
+}))
 
 const About = () => {
+  const classes = useStyles();
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '98vh'
-      }}
-    >
-      <h1>About</h1>
+    <div className={classes.container}>
+      <div className={classes.about}>
+        <Typography variant="h1">Until the day you <span style={{color: "#9c27b0"}}>bloom</span></Typography>
+        <Typography variant="body3">&nbsp;</Typography>
+        <Typography variant="body1">description</Typography>
+      </div>
+      <img className={classes.imageContainer} src={image} width="auto" />
     </div>
   );
 }
