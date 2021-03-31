044c3e7b12e9d1fdcce01b410e70566eba0a79f4
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-03-31 18:44:05 +0900

    Remove unnecessary files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import purple from '@material-ui/core/colors/purple';
@@ -11,11 +10,9 @@ const theme = createMuiTheme({
   palette: {
     primary: { 
       main: '#000',
-      // contrastText: '#fff',
     },
     secondary: { 
       main: purple[500], 
-      // contrastText: '#fff', 
     },
   }, 
   typography: {
@@ -49,8 +46,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
