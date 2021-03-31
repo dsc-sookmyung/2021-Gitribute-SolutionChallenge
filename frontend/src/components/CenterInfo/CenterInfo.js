5b7e292002a72e55b06a28bd3f45e933b4329c85
0hee0 <53266682+0hee0@users.noreply.github.com>
2021-03-31 20:36:28 +0900

    Fix Responsive UI

--- frontend/src/components/CenterInfo/CenterInfo.js
+++ frontend/src/components/CenterInfo/CenterInfo.js
@@ -10,17 +10,15 @@ import CenterTable from './CenterTable';
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    backgroundColor: "#fff",
-    height: "100vh",
-    padding: "2.5rem calc((100vw - 1193px) / 2 + 1rem)",
+    padding: "6.5rem calc((100vw - 1193px) / 2 + 1rem)",
     display: "flex",
     flexDirection: "column",
     alignItems: "flex-start",
     '@media screen and (max-width: 1193px)': {
-      padding: "2.5rem 1rem"
+      padding: "6.5rem 1rem"
     },
     '@media screen and (max-width:767px)': {
-      padding: "2.5rem 2rem",
+      padding: "6.5rem 2rem",
       flexDirection: "column"
     },
   },
@@ -69,19 +67,23 @@ export default function CenterInfo() {
   useEffect(async () => {
     const defaultCenterInfo = await UserService.getDefaultCenter(region)
     if (defaultCenterInfo.center) {
-      console.log("defaultCenter: "+JSON.stringify(defaultCenterInfo));
+      console.log("defaultCenterInfo: "+JSON.stringify(defaultCenterInfo));
       setCenterNames(defaultCenterInfo.center);
       setDefaultCenter(defaultCenterInfo);
     }
     else {
       setCenterNames({ center: "Center is in preparation!" });
-      setDefaultCenter({"lat": 0.0, "lng": 0.0, "pads": {}, "password": null, "phonenumber": null, "location": null})
+      setDefaultCenter({"lat": 0.0, "lng": 0.0, "pads": {"liner": 0, "medium": 0, "large": 0, "overnight": 0}, "password": "password", "phonenumber": "phone number", "location": "location"})
     }
   }, [region]);
 
   useEffect(() => {
     console.log("Centernames: "+JSON.stringify(centerNames));
   }, [centerNames]);
+
+  useEffect(() => {
+    console.log("DefaultCenter: "+JSON.stringify(defaultCenter));
+  }, [defaultCenter]);
 
   const changeRegion = (e) => {
     setRegion(parseInt(e.target.value, 10));
@@ -100,49 +102,49 @@ export default function CenterInfo() {
           onChange={changeRegion}
           className={classes.radio}  
         >
-          <Grid item xs={6} sm={3}>
+          <Grid item sm={6} md={3}>
           <FormControlLabel
             value="0"
             control={<Radio />}
             label="All"
           />
           </Grid>
-          <Grid item xs={6} sm={3}>
+          <Grid item sm={6} md={3}>
           <FormControlLabel
             value="1" 
             control={<Radio />}
             label="Seoul"
           />
           </Grid>
-          <Grid item xs={6} sm={3}>
+          <Grid item sm={6} md={3}>
           <FormControlLabel
             value="2"
             control={<Radio />}
             label="Gyeonggi/Incheon"
           />
           </Grid>
-          <Grid item xs={6} sm={3}>
+          <Grid item sm={6} md={3}>
           <FormControlLabel
             value="3"
             control={<Radio />}
             label={"Chungnam/Sejong/\nDaejeon/Gangwon"}
           />
           </Grid>
-          <Grid item xs={6} sm={3}>
+          <Grid item sm={6} md={3}>
           <FormControlLabel
             value="4" 
             control={<Radio />}
             label="Jeolla/Gwangju"
           />
           </Grid>
-          <Grid item xs={6} sm={3}>
+          <Grid item sm={6} md={3}>
           <FormControlLabel
             value="5" 
             control={<Radio />}
             label={"Gyeongsang/Daegu/\nBusan/Ulsan"}
           />
           </Grid>
-          <Grid item xs={6} sm={3}>
+          <Grid item sm={6} md={3}>
           <FormControlLabel
             value="6" 
             control={<Radio />}
@@ -150,7 +152,7 @@ export default function CenterInfo() {
           />
           </Grid>
           {/*
-          <Grid item xs={6} sm={3}>
+          <Grid item sm={6} md={3}>
           <FormControlLabel 
             value="7" 
             disabled control={<Radio />} 
