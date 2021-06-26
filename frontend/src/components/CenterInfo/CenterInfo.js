7d0de49c608112361f56059c3b00b9f09577d0a0
0hee0 <suhee0206@gmail.com>
2021-06-27 01:59:55 +0900

    Implement Finding nearest center & Search by center name

--- frontend/src/components/CenterInfo/CenterInfo.js
+++ frontend/src/components/CenterInfo/CenterInfo.js
@@ -6,6 +6,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { Radio, RadioGroup, FormControl, FormControlLabel } from '@material-ui/core';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import SearchIcon from '@material-ui/icons/Search';
+import RoomIcon from '@material-ui/icons/Room';
 import CenterTable from './CenterTable';
 
 const useStyles = makeStyles((theme) => ({
@@ -26,7 +31,7 @@ const useStyles = makeStyles((theme) => ({
   },
   select: {
     marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(6),
+    marginBottom: theme.spacing(3),
   },
   table: {
     maxWidth: "1194px",
@@ -38,15 +43,37 @@ const useStyles = makeStyles((theme) => ({
   submit: {
     margin: theme.spacing(6, 0, 2),
   },
+  selectCurrent: {
+    paddingRight: "0.5rem",
+  },
+  
 }));
+
+function MarkerRadio(props) {
+  return (
+    <Radio
+      sx={{
+        '&:hover': {
+          bgcolor: 'transparent',
+        },
+      }}
+      disableRipple
+      color="default"
+      checkedIcon={<RoomIcon style={{ color: "#9c27b0" }} />}
+      icon={<RoomIcon />}
+      {...props}
+    />
+  );
+}
 
 export default function CenterInfo({ handleUpdate }) {
   const [role, setRole] = useState(0);
   const [currentUser, setCurrentUser] = useState(undefined);
   const [star, setStar] = useState(undefined);
   const [region, setRegion] = useState(0);
-  const [centerNames, setCenterNames] = useState({});
+  const [centerNames, setCenterNames] = useState([]);
   const [defaultCenter, setDefaultCenter] = useState({});
+  const [searchName, setSearchName] = useState("");
   const classes = useStyles();
 
   useEffect(() => {
@@ -67,17 +94,45 @@ export default function CenterInfo({ handleUpdate }) {
   }, [currentUser]);
 
   useEffect(async () => {
-    const defaultCenterInfo = await UserService.getDefaultCenter(region)
-    if (defaultCenterInfo.center) {
-      console.log("defaultCenterInfo: "+JSON.stringify(defaultCenterInfo));
-      setCenterNames(defaultCenterInfo.center);
-      setDefaultCenter(defaultCenterInfo);
+    if (region === 7) {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const defaultCenterInfo = UserService.getNearestCenter(position.coords.latitude, position.coords.longtitude);
+            console.log("defaultCenterInfo: "+JSON.stringify(defaultCenterInfo));
+            setCenterNames(defaultCenterInfo.center);
+            setDefaultCenter(defaultCenterInfo);    
+          },
+          () => {
+            handleLocationError(true);
+          }
+        )
+      }
+      else {
+        // Brower doesn't support Geolocation
+        handleLocationError(false);
+      }
     }
     else {
-      setCenterNames({ center: "Center is in preparation!" });
-      setDefaultCenter({"lat": 0.0, "lng": 0.0, "pads": {"liner": 0, "medium": 0, "large": 0, "overnight": 0}, "password": "password", "phonenumber": "phone number", "location": "location"})
+      const defaultCenterInfo = await UserService.getDefaultCenter(region)
+      if (defaultCenterInfo.center) {
+        console.log("defaultCenterInfo: "+JSON.stringify(defaultCenterInfo));
+        setCenterNames(defaultCenterInfo.center);
+        setDefaultCenter(defaultCenterInfo);
+      }
+      else {
+        setCenterNames({ center: "Center is in preparation!" });
+        setDefaultCenter({"lat": 0.0, "lng": 0.0, "pads": {"liner": 0, "medium": 0, "large": 0, "overnight": 0}, "password": "password", "phonenumber": "phone number", "location": "location"})
+      }
     }
   }, [region]);
+
+  function handleLocationError(browserHasGeolocation) {
+    let infoWindowContent = ""
+      browserHasGeolocation
+    ?  infoWindowContent = "Error: The Geolocation service failed."
+    :  infoWindowContent = "Error: Your browser doesn't support geolocation."
+  }  
 
   useEffect(() => {
     console.log("Centernames: "+JSON.stringify(centerNames));
@@ -87,15 +142,57 @@ export default function CenterInfo({ handleUpdate }) {
     console.log("DefaultCenter: "+JSON.stringify(defaultCenter));
   }, [defaultCenter]);
 
+  useEffect(() => {
+    console.log("searchName: "+searchName);
+  }, [searchName]);
+
   const changeRegion = (e) => {
     setRegion(parseInt(e.target.value, 10));
   }
 
+  const handleSearch = (event, value) => {
+    setSearchName(value);
+  }
+
   return (
     <div className={classes.container}>
-      <Typography variant="h3">
-        Search by location
-      </Typography>
+      <Grid container>
+        <Grid item xs={6} md={9}>
+        <Typography variant="h3">
+          Search by area
+        </Typography>
+        </Grid>
+        <Grid item xs={6} md={3}>
+        <Autocomplete
+          onChange={handleSearch}
+          Search by center name
+          id="search-name"
+          options={centerNames.map((center) => {return (center);})}
+          renderInput={(params) => (
+            <TextField 
+              {...params} 
+              label="Search by center name" 
+              variant="outlined"
+              InputProps={{
+                ...params.InputProps,
+                startAdornment:
+                <>
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                  {params.InputProps.startAdornment}
+                </>
+              }}
+              onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                  e.preventDefault();
+                }
+              }}
+            />
+          )}
+        />
+        </Grid>
+      </Grid>
       <Grid container className={classes.select}>
         <FormControl component="fieldset">
         <RadioGroup 
@@ -153,12 +250,20 @@ export default function CenterInfo({ handleUpdate }) {
             label="Jeju"
           />
           </Grid>
+          <Grid item sm={6} md={3}>
+          <FormControlLabel
+            className={classes.selectCurrent}
+            value="7" 
+            control={<MarkerRadio />}
+            label="Current Location Center"
+          />
+          </Grid>
         </RadioGroup>
         </FormControl>
       </Grid>
       <CenterTable className={classes.table} currentUser={currentUser} role={role} 
       region={region} star={star} centerNames={centerNames} defaultCenter={defaultCenter}
-      handleUpdate={handleUpdate} />
+      handleUpdate={handleUpdate} searchName={searchName} />
     </div>
   );
 }
